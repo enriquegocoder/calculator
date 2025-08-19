c04d583e20f917926e736c6ab3612f344bdc5507
Enrique Gonzalez Duque <63886300+enriquegocoder@users.noreply.github.com>
2025-08-19 04:17:53 -0500

    Calculadora React

---
+++ src/components/Switch.jsx
@@ -0,0 +1,26 @@
+import { useEffect, useState } from 'react'
+import './switch.css'
+
+const Switch = () => {
+
+  const [theme, setTheme] = useState('claro')
+
+  const handleChange = (e) => setTheme(e.target.checked ? 'oscuro' : 'claro')
+
+  useEffect(() => {
+    document.body.setAttribute('data-theme', theme)
+  }, [theme])
+  
+
+  return (
+    <section className='switch'>
+      <label className='toggle'>
+        <input type="checkbox" className='check-switch' onChange={handleChange} hidden />
+        <span className='slider'></span>
+      </label>
+
+    </section>
+  )
+}
+
+export default Switch
